b8f3ed416322be8ef40f2d3fccabe0a3ee4d5459
brandonrodriguez6 <37849270+brandonrodriguez6@users.noreply.github.com>
2019-01-25 15:24:54 -0800

    Add files via upload

---
+++ javabrandon/script.js
@@ -0,0 +1,7 @@
+// if you want to change something on a DIFFERENT element, you have to call it by name (it's "id")
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+	
+// "el" stands for the "element" that was just clicked
+function showMax(el){
+	document.getElementById("shit").src = "image/max.jpg";
+}
